d666db519beeb88102fd3fbfc53be69eda691296
Andrey Govorukhin <andr.govorukhin@gmail.com>
2020-06-20 13:38:54 +0100

    Добавил компонент Loader

---
+++ src/components/app/Loader.vue
@@ -0,0 +1,66 @@
+<template>
+  <div
+    class="loader"
+    :class="{
+      'loader--white': isWhite,
+    }"
+  >
+    <div class="loader__circle"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loader',
+  props: {
+    isWhite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.loader {
+  position: relative;
+  display: flex;
+  align-items: center;
+  color: $accent;
+
+  &::after,
+  &::before {
+    content: '';
+  }
+
+  &__circle,
+  &::after,
+  &::before {
+    background-color: currentColor;
+    display: block;
+    border-radius: 50%;
+    height: 8px;
+    width: 8px;
+    animation-name: scale;
+    animation-duration: 600ms;
+    animation-iteration-count: infinite;
+  }
+
+  &::before {
+    animation-delay: 100ms;
+  }
+
+  &__circle {
+    margin: 6px;
+    animation-delay: 200ms;
+  }
+
+  &::after {
+    animation-delay: 300ms;
+  }
+  // --white
+  &--white {
+    color: $white;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 
+import Loader from '@/components/app/Loader';
+Vue.component('Loader', Loader);
 Vue.config.productionTip = false;
 
 new Vue({
