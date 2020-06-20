557f4824c24d7a74f46e5cea9e56c347747a9ade
Andrey Govorukhin <andr.govorukhin@gmail.com>
2020-06-20 13:39:10 +0100

    Добавил компонент Icon

---
+++ src/components/app/Icon.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg>
+    <use :xlink:href="`#${name}`" />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      default: '',
+      type: String,
+    },
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue';
 import store from './store';
 
 import Loader from '@/components/app/Loader';
+import Icon from '@/components/app/Icon';
+
 Vue.component('Loader', Loader);
+Vue.component('Icon', Icon);
 Vue.config.productionTip = false;
 
 new Vue({
