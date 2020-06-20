c2da16603cd454e6874e0713bf3f5d6436f06966
Andrey Govorukhin <andr.govorukhin@gmail.com>
2020-06-20 13:17:40 +0100

    Заготовка табов

---
+++ src/components/Authentication.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="authentication">
+    <div v-if="isHeader" class="authentication__header">
+      <button
+        v-for="tab in tabs"
+        :key="tab.name"
+        :class="[
+          'authentication__header-tab',
+          { 'authentication__header-tab--active': currentTab === tab.name },
+        ]"
+        @click="currentTab = tab.name"
+      >
+        {{ tab.text }}
+      </button>
+    </div>
+    <div class="authentication__body">
+      <keep-alive>
+        <component v-bind:is="currentTabComponent" class="tab"></component>
+      </keep-alive>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'authentication',
+  components: {
+    Login: () => import('@/components/Login'),
+    Registration: () => import('@/components/Registration'),
+  },
+  data: () => ({
+    isHeader: true,
+    tabs: [
+      { text: 'Вход', name: 'login' },
+      { text: 'Регистрация', name: 'registration' },
+    ],
+    currentTab: 'registration',
+  }),
+  computed: {
+    currentTabComponent: function() {
+      return this.currentTab;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.authentication {
+  width: 100%;
+  max-width: 407px;
+  margin: auto;
+
+  &__header {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  &__header-tab {
+    cursor: pointer;
+    color: #b1b1b1;
+    border-radius: 4px;
+    margin-right: 4px;
+    width: 100%;
+    text-align: center;
+    padding: 18px 0 24px;
+    background-color: transparent;
+    border: solid #e8e8e8;
+    border-width: 1px;
+    text-transform: uppercase;
+    font-weight: 600;
+    font-size: rem(11);
+    letter-spacing: 0.05em;
+
+    &--active {
+      background-color: $white;
+      border-color: $white;
+      color: $accent;
+    }
+
+    &:last-of-type {
+      margin-right: 0;
+    }
+  }
+
+  &__body {
+    border-radius: 4px;
+    background: $white;
+    position: relative;
+    top: -6px;
+  }
+}
+</style>
---
+++ src/components/Registration.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="registration">
+    registration
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'registration',
+};
+</script>
+
+<style></style>
