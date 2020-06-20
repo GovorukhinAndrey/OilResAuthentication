73ad4a7e70348d769ab9f9f13560dcd1f707c701
Andrey Govorukhin <andr.govorukhin@gmail.com>
2020-06-20 15:33:36 +0100

    Компонент Чекбокса

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="login">
     login
+    <Checkbox v-model="isRemember" text="Запомнить меня"></Checkbox>
   </div>
 </template>
 
 <script>
 export default {
   name: 'login',
+  components: {
+    Checkbox: () => import('@/components/Checkbox'),
+  },
+  data: () => ({
+    isRemember: false,
+  }),
 };
 </script>
 
