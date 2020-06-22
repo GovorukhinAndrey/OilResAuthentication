49d1333cf085a03658107adb1ae805fff5273e0d
Andrey Govorukhin <andr.govorukhin@gmail.com>
2020-06-22 22:24:11 +0100

    Добавил svg иконки

--- vue.config.js
+++ vue.config.js
@@ -7,4 +7,11 @@ module.exports = {
       },
     },
   },
+
+  chainWebpack: config => {
+    config.module
+      .rule('svg-sprite')
+      .use('svgo-loader')
+      .loader('svgo-loader');
+  },
 };
