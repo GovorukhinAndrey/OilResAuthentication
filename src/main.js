c2b0359b92909918ed59b2a58500fded99c9fcea
Andrey Govorukhin <andr.govorukhin@gmail.com>
2020-06-23 02:54:28 +0100

    Подключил валидацию

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueMask from 'v-mask';
 import VueSwal from 'vue-swal';
+import Vuelidate from 'vuelidate';
 import VueTippy, { TippyComponent } from 'vue-tippy';
 import App from './App.vue';
 import store from './store';
@@ -10,6 +11,7 @@ import { firebaseAuth } from '@/firebase';
 Vue.use(VueSwal);
 Vue.use(VueMask);
 Vue.use(VueTippy);
+Vue.use(Vuelidate);
 
 Vue.component('tippy', TippyComponent);
 Vue.config.productionTip = false;
