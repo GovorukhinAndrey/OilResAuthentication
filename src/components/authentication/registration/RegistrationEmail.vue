2c06ed667a2bd106503c3fc74ad4d05f3971707a
Andrey Govorukhin <andr.govorukhin@gmail.com>
2020-06-23 02:53:15 +0100

    Валидация регистрации по email

--- src/components/authentication/registration/RegistrationEmail.vue
+++ src/components/authentication/registration/RegistrationEmail.vue
@@ -1,10 +1,24 @@
 <template>
   <form @submit.prevent="handlerSubmit" class="form">
+    <small v-if="$v.fullName.$dirty && $v.fullName.$error" class="invalid">
+      Поле ФИО не должно быть пустым
+    </small>
+    <small v-if="$v.phone.$dirty && $v.phone.$error" class="invalid">
+      Введите корректный телефон
+    </small>
+    <small v-if="$v.email.$dirty && $v.email.$error" class="invalid">
+      Введите корректный Email
+    </small>
+    <small v-if="$v.password.$dirty && $v.password.$error" class="invalid">
+      Введите больше {{ $v.password.$params.minLength.min }} символов. Сейчас он
+      {{ password.length }}
+    </small>
     <InputControl
       class="input-control"
       tooltip="Укажите ФИО"
-      label="Телефон"
+      label="ФИО"
       placeholder="Константинопольский Константин Конст..."
+      v-model="fullName"
     />
     <div class="input-control-group">
       <InputControl
@@ -13,6 +27,7 @@
         label="Телефон"
         placeholder="90000000"
         type="phone"
+        v-model="phone"
       />
       <InputControl
         class="input-control input-control-group__item"
@@ -20,6 +35,7 @@
         label="Email"
         placeholder="exsample@mail.com"
         type="email"
+        v-model="email"
       />
     </div>
     <InputControl
@@ -28,19 +44,71 @@
       label="Пароль"
       placeholder="_"
       type="password"
+      v-model="password"
     />
 
-    <ButtonControl class="form__btn" isBlock size="large" type="secondary" loading>
+    <ButtonControl
+      nativeType="submit"
+      class="form__btn"
+      isBlock
+      size="large"
+      type="secondary"
+      :loading="loading"
+    >
       Регистрация
     </ButtonControl>
   </form>
 </template>
 
 <script>
+import { email, required, minLength } from 'vuelidate/lib/validators';
+import { mapActions } from 'vuex';
+
 export default {
+  name: 'RegistrationEmail',
   components: {
     InputControl: () => import('@/components/app/InputControl'),
     ButtonControl: () => import('@/components/app/ButtonControl'),
+  },
+  validations: {
+    email: { email, required },
+    password: { required, minLength: minLength(6) },
+    fullName: { required },
+    phone: { minLength: minLength(17) },
+  },
+  data: () => ({
+    loading: false,
+    email: '',
+    password: '',
+    phone: '',
+    fullName: '',
+  }),
+  methods: {
+    ...mapActions('auth', ['registerForEmail']),
+    async handlerSubmit() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+      const formData = {
+        email: this.email,
+        password: this.password,
+        phone: this.phone,
+        fullName: this.fullName,
+      };
+      try {
+        this.loading = true;
+        await this.registerForEmail(formData);
+      } catch (e) {
+        throw e.message;
+      } finally {
+        this.loading = false;
+        this.email = '';
+        this.password = '';
+        this.phone = '';
+        this.fullName = '';
+      }
+    },
   },
 };
 </script>
